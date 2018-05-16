785f0a6b7c82ed2e07bb94f845cbfe8360e1f4ec
薛定谔的猫 <hh_2013@foxmail.com>
2018-05-16 14:52:54 +0800

    Chore: switch to eslint-config-eslint (#9)

--- bin/eslint.js
+++ bin/eslint.js
@@ -3,28 +3,28 @@
  * @author Toru Nagashima
  * See LICENSE file in root directory for full license.
  */
-"use strict"
+"use strict";
 
-const debug = require("debug")("eslint-cli")
-const debugMode = process.argv.indexOf("--debug") !== -1
-const cwd = process.cwd()
+const debug = require("debug")("eslint-cli");
+const debugMode = process.argv.indexOf("--debug") !== -1;
+const cwd = process.cwd();
 
 if (debugMode) {
-    require("debug").enable("eslint-cli")
+    require("debug").enable("eslint-cli");
 }
 
-debug("START", process.argv)
-debug("ROOT", cwd)
+debug("START", process.argv);
+debug("ROOT", cwd);
 
-const binPath = require("../lib/get-local-eslint")(cwd) || require("../lib/get-bin-eslint-js")(cwd)
-if (binPath != null) {
-    require(binPath)
-}
-else {
-    //eslint-disable-next-line no-console
+const binPath = require("../lib/get-local-eslint")(cwd) || require("../lib/get-bin-eslint-js")(cwd);
+
+if (binPath !== null) {
+    require(binPath);
+} else {
+    // eslint-disable-next-line no-console
     console.error(`
 Could not find local ESLint.
 Please install ESLint by 'npm install --save-dev eslint'.
-`)
-    process.exitCode = 1
+`);
+    process.exitCode = 1;
 }
